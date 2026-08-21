d168b6b3b9f107245f91c0e2e1ebe95a3599f4a9
gnawnah <studyaccforwang@gmail.com>
2026-08-21 22:49:52 +1200

    initial commit, starting alu

---
+++ rtl/alu.sv
@@ -0,0 +1,15 @@
+module alu(
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [3:0] ctrl,
+    output logic [31:0] output
+    output logic [31:0] zero
+);
+
+    
+
+
+
+
+
+endmodule
