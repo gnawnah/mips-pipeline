006b7019c7d8b853ea5cda438fbdbcf69b37b0a2
gnawnah <studyaccforwang@gmail.com>
2026-08-25 17:06:41 +1200

    add register file with dual read ports

---
+++ rtl/regfile.sv
@@ -0,0 +1,17 @@
+module regfile(
+    input logic [4:0] read_reg1,
+    input logic [4:0] read_reg2,
+    input logic [4:0] write_reg,
+    input logic [31:0] write_data,
+    input logic RegWrite,
+    input logic clk,
+    output logic [31:0] read_data1,
+    output logic [31:0] read_data2
+);
+
+
+
+
+
+
+endmodule
