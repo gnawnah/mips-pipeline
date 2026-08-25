010397fde3e7dcb163d98ef3105692a7cd7f194e
gnawnah <studyaccforwang@gmail.com>
2026-08-25 17:19:09 +1200

    fixed typo, ensure when read reg0 it always returns 0

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -11,8 +11,8 @@ module regfile(
 
     logic [31:0] regs [0:31]; // registers
 
-    always_ff @(poesedge clk) begin
-        if(RegWrite) begin
+    always_ff @(posedge clk) begin
+        if(RegWrite && write_reg !=0) begin
             regs[write_reg] <= write_data;
         end
 
@@ -20,8 +20,8 @@ module regfile(
     end
 
     always_comb begin
-        read_data1 = regs[read_reg1];
-        read_data2 = regs[read_reg2];
+        read_data1 = (read_reg1 == 0) ? 0 : regs[read_reg1];
+        read_data2 = (read_reg2 == 0) ? 0 : regs[read_reg2];
     end
 
 
