fe562558dd04d5ee466d6c72513cd2a4e43fb053
gnawnah <studyaccforwang@gmail.com>
2026-08-25 16:57:20 +1200

    change ctrl bits from 4 to 3

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,17 +1,17 @@
 module alu(
     input logic signed [31:0] a,
     input logic signed [31:0] b,
-    input logic [3:0] ctrl,
+    input logic [2:0] ctrl,
     output logic [31:0] result,
     output logic zero = 0
 );  
     always_comb begin
         case (ctrl)
-            4'b0000: result = a & b;
-            4'b0001: result = a | b;
-            4'b0010: result = a + b;
-            4'b0110: result = a - b;
-            4'b0111: result = a < b;
+            3'b000: result = a & b;
+            3'b001: result = a | b;
+            3'b010: result = a + b;
+            3'b110: result = a - b;
+            3'b111: result = a < b;
             default: result = 0;
         endcase
         if(result==0) zero = 1;
--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -1,13 +1,13 @@
 module alu_tb;
     logic signed [31:0] a;
     logic signed [31:0] b;
-    logic [3:0] ctrl;
+    logic [2:0] ctrl;
     logic [31:0] result;
     logic zero;
 
     alu u0 (.a(a), .b(b), .ctrl(ctrl), .result(result), .zero(zero));
 
-    task check(input [31:0] a_t, b_t, input [3:0] ctrl_t, input [31:0] expected_result, input expected_zero);
+    task check(input [31:0] a_t, b_t, input [2:0] ctrl_t, input [31:0] expected_result, input expected_zero);
         begin
             a = a_t;
             b = b_t;
@@ -29,15 +29,15 @@ module alu_tb;
         $dumpvars(0, alu_tb);
 
         // check and
-        check(1,1,4'b0000,1,0);
+        check(1,1,3'b000,1,0);
         // check or
-        check(1,0,4'b0001,1,0);
+        check(1,0,3'b001,1,0);
         // check slt
-        check(1,2,4'b0111,1,0);
+        check(1,2,3'b111,1,0);
         // check add for zero
-        check(0,0,4'b0010,0,1);
+        check(0,0,3'b010,0,1);
         // check add again, zero should drop back to 0
-        check(5,7,4'b0010,12,0);
+        check(5,7,3'b010,12,0);
 
         $finish;
 
