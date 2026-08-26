ad61cf50b1b499fa25b2041389f7f36880717662
gnawnah <studyaccforwang@gmail.com>
2026-08-26 13:25:16 +1200

    main_control done, mapped output based on opcode, set dont care states to 0

--- rtl/main_control.sv
+++ rtl/main_control.sv
@@ -31,12 +31,43 @@ module main_control(
                 MemRead = 1'b1;
                 MemWrite = 1'b0;
                 Branch = 1'b0;
-                ALUOp = 2'b10;
+                ALUOp = 2'b00;
             end
             6'b101011:
+            begin
+                RegDst = 1'b0;
+                ALUSrc = 1'b1;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b0;
+                MemRead = 1'b0;
+                MemWrite = 1'b1;
+                Branch = 1'b0;
+                ALUOp = 2'b00;
+            end
             6'b000100:
+            begin
+                RegDst = 1'b0;
+                ALUSrc = 1'b0;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b0;
+                MemRead = 1'b0;
+                MemWrite = 1'b0;
+                Branch = 1'b1;
+                ALUOp = 2'b01;
+            end
             default:
+            begin
+                RegDst = 1'b0;
+                ALUSrc = 1'b0;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b0;
+                MemRead = 1'b0;
+                MemWrite = 1'b0;
+                Branch = 1'b0;
+                ALUOp = 2'b00;
+            end
         endcase
+    end
 
 
 
