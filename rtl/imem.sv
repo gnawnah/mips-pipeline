942545aab990bf66659734759d5b971fe0caa27a
gnawnah <studyaccforwang@gmail.com>
2026-08-26 23:48:25 +1200

    add instruction memory with  load

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -5,6 +5,10 @@ module imem(
     // this array stores 256 words instead of 1024 bytes
     logic [31:0] mem [0:255]; 
 
+    initial $readmemh("program.hex", mem);
+
+    // [9:2] since we're accessing words instead of bytes
+    assign instruction = mem[addr[9:2]];
 
 
 endmodule
