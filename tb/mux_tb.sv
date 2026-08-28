808f5935699bffd7a4807e5d98586cc6d56604a1
gnawnah <studyaccforwang@gmail.com>
2026-08-28 16:19:57 +1200

    add mux testbench, parameterised for 32bits and 5bits

--- tb/mux_tb.sv
+++ tb/mux_tb.sv
@@ -1,19 +1,33 @@
-module mux_tb #(parameter WIDTH = 32);
-    logic [WIDTH-1:0] in0;
-    logic [WIDTH-1:0] in1;
+module mux_tb;
     logic sel;
-    logic [WIDTH-1:0] out;
+    // 32 bit
+    logic [31:0] in0, in1, out;
+    // 5 bit for RegDst
+    logic [4:0] a5,b5,c5;
 
-    mux #(.WIDTH(WIDTH)) dut(.in0(in0),.in1(in1),.sel(sel),.out(out));
+    mux #(.WIDTH(32)) dut32 (.in0(in0),.in1(in1),.sel(sel),.out(out));
 
-    task check(input [WIDTH-1:0] in0_t, input [WIDTH-1:0] in1_t, input sel_t, input [WIDTH-1:0] expected);
+    mux #(.WIDTH(5)) dut5 (.in0(a5),.in1(b5),.sel(sel),.out(c5));
+
+    task check32(input [31:0] x, input [31:0] y, input sel_t, input [31:0] expected);
     begin
-        in0 = in0_t;
-        in1 = in1_t;
+        in0 = x;
+        in1 = y;
         sel = sel_t;
         #1;
         if(out==expected) $display("YES");
-        else $display("NO");
+        else $display("NO, out=%h,exp=%h",out,expected);
+    end
+    endtask
+
+    task check5(input [4:0] a, input [4:0] b, input sel_t, input [4:0] expected);
+    begin
+        a5 = a;
+        b5 = b;
+        sel = sel_t;
+        #1;
+        if(c5==expected) $display("YES");
+        else $display("NO, out=%h,exp=%h",c5,expected);
     end
     endtask
 
@@ -21,11 +35,15 @@ module mux_tb #(parameter WIDTH = 32);
         $dumpfile("mux.vcd");
         $dumpvars(0,mux_tb);
 
-        // check sel=0
-        check(32'h0000_1000,32'h0000_2000,1'b0,32'h0000_1000);
+        // 32 bits: check sel=0
+        check32(32'h0000_1000,32'h0000_2000,1'b0,32'h0000_1000);
+        // 32 bits:check sel=1
+        check32(32'h0000_3000,32'h0000_3008,1'b1,32'h0000_3008);
 
-        check(32'h0000_3000,32'h0000_3008,1'b1,32'h0000_3008);
-
+        // 5 bits: check sel=0
+        check5(5'b00010,5'b00011,1'b0,5'b00010);
+        // 5 bits: check sel=1
+        check5(5'b10001,5'b11100,1'b1,5'b11100);
         $finish;
     end
 
