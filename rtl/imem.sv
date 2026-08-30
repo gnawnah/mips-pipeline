ca5162727f0ec84d638e7100957b49171e0a6025
gnawnah <studyaccforwang@gmail.com>
2026-08-30 16:17:27 +1200

    add j to progam.hex, imem read from programs folder

--- rtl/imem.sv
+++ rtl/imem.sv
@@ -5,7 +5,7 @@ module imem(
     // this array stores 256 words instead of 1024 bytes
     logic [31:0] mem [0:255]; 
 
-    initial $readmemh("program.hex", mem);
+    initial $readmemh("test/program.hex", mem);
 
     // [9:2] since we're accessing words instead of bytes
     assign instruction = mem[addr[9:2]];
