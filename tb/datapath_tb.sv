a00ebc9262cbbc969cd8fb665569cea9c1ae3b34
gnawnah <studyaccforwang@gmail.com>
2026-09-01 11:47:02 +1200

    add memory check task and call checks

--- tb/datapath_tb.sv
+++ tb/datapath_tb.sv
@@ -15,29 +15,36 @@ module datapath_tb;
         forever #5 clk = ~clk;
     end
 
+    // for these two funnctions, pure read and compare, no driving signals, they already exist
     task check_reg(input [4:0] r, input [31:0] expected, input string name);
         begin
-            if(dut.u_regfile.regs[r] === exp) begin
+            if(cpu.u_regfile.regs[r] === expected) begin
                 passes++;
-                $display("PASS, %s", nm);
+                $display("PASS, %s", name);
             end
             else begin
                 fails++;
-                $display("FAIL, %s", nm);
+                $display("FAIL, %s: got %0d exp %0d", name, cpu.u_regfile.regs[r], expected);
             end
         end
     endtask
 
     task check_mem(input int index, input [31:0] expected, input string name);
         begin
-
-
+            if(cpu.u_dmem.mem[index] === expected) begin
+                passes++;
+                $display("PASS, %s", name);
+            end
+            else begin
+                fails++;
+                $display("FAIL, %s: got %0d exp %0d", name, cpu.u_dmem.regs[index], expected);
+            end
         end
     endtask
 
     initial begin
         $dumpfile("datapath.vcd");
-        $dumpvars(0,data_tb);
+        $dumpvars(0,datapath_tb);
 
         reset = 1;
         @(posedge clk);
@@ -47,6 +54,19 @@ module datapath_tb;
         repeat(16) @(posedge clk);
 
         #1;
+
+        check_reg(8, 5, "$8 addi");
+        check_reg(9, 3, "$9 addi");
+        check_reg(10, 8, "$10 add (5+3)");
+        check_reg(11, 2, "$11 sub (5-3)");
+        check_reg(12, 1, "#12 and (5&3)");
+        check_reg(13, 7, "$13 or (5|3)");
+        check_reg(14, 1, "$14 slt (3<5)");
+        check_reg(15, 8, "$15 lw reads what sw wrote");
+        check_reg(16, 7, "$16 beq skipped");
+        check_reg(17, 4, "j skipped");
+
+        check_mem(0, 8, "mem [0] sw");
 
         $finish;
 
