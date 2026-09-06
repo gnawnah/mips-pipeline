e49640149bb2d9daa7e4f437dd6d0eceebd5c398
gnawnah <studyaccforwang@gmail.com>
2026-09-06 22:27:55 +1200

    add pipeline register with stall/flush support

---
+++ rtl/pipeline_reg.sv
@@ -0,0 +1,15 @@
+module pipeline_reg #(parameter WIDTH = 32)(
+    input logic clk,
+    input logic stall, // when high, register holds its current value, does not latch new input
+    input logic flush, // when high, register clears to zero
+    input logic [WIDTH-1:0] in,
+    output logic [WIDTH-1:0] out
+);
+
+    always_ff @(posedge clk ) begin
+        if(flush) out<=0;
+        else if (stall) out<=out;
+        else out<=in;
+    end
+
+endmodule
